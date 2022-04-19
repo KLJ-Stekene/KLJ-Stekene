175745aec2418adefebc64de287c3c054d5372d1
Jens Penneman <jenspenneman26@gmail.com>
2022-04-19 20:53:50 +0200

    Removed commentary from the homepage container
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/containers/startpagina/index.jsx
+++ src/containers/startpagina/index.jsx
@@ -20,23 +20,6 @@ const Page = memo(() => {
 						</ImageListItem>)
 					})
 			}
-			{/*
-			<ImageListItem>
-				<img src={WeekendGroepsfoto2022} alt={"Groepsfoto van op weekend in het jaar 2022"} loading={"lazy"}
-					 width={1669} height={938}/>
-			</ImageListItem>
-			<ImageListItem>
-				<img src={Sportfeest19Foto} alt={"Sportfeest foto van het jaar 2019"} loading={"lazy"} width={3871}
-					 height={2177}/>
-			</ImageListItem>
-			<ImageListItem>
-				<img src={Bestuursfoto2021} alt={"Bestuur groepsfoto van het jaar 2021"} loading={"lazy"}
-					 width={3021} height={1699}/>
-			</ImageListItem>
-			<ImageListItem>
-				<img src={Sportfeest21Foto} alt={"Sportfeestfoto van het jaar 2021"} loading={"lazy"} width={1442}
-					 height={811}/>
-			</ImageListItem>*/}
 		</ImageList>
 		<Nieuws />
 		<Gemakske />
