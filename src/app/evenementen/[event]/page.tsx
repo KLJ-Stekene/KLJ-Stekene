b4faa588c58dd2055690bf311332d56af8db7182
Jens Penneman <jenspenneman26@gmail.com>
2023-06-13 18:01:43 +0200

    Eventpage info added
    TODO:
    - Sponsors
    - Google maps
    - Line-up
    ...
    
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/app/evenementen/[event]/page.tsx
+++ src/app/evenementen/[event]/page.tsx
@@ -1,6 +1,8 @@
 import {Metadata} from "next";
 import siteConfig from "@/site.config";
 import {notFound} from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     params: { event: string }
@@ -59,10 +61,38 @@ export async function generateMetadata({params}: Props): Promise<Metadata> {
     };
 }
 
-export default function EvenementenNL() {
+export default function EvenementenNL({params}: Props) {
+    const eventURL = params.event;
+    const event = siteConfig.evenementen.find((event) => event.URL === eventURL);
+    if (event === undefined) notFound();
+
     return <main className={"min-h-[calc(100svh-72px-56px)] print:min-h[100svh] flex flex-col gap-2"}>
-        <section className={"container mx-auto px-2"}>
-            <p className={"bg-yellow-100 dark:bg-yellow-900 rounded-lg p-2"}>De website is nog niet klaar! Kom binnen enkele dagen terug voor het evenement!</p>
+        <section className={"container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 gap-2 px-2"}>
+            <Image className={"w-full min-h-[35svh] object-cover sm:col-span-2 lg:col-span-3 print:col-span-3 rounded-lg"} priority src={event.images.banner} alt={"Event's image"} width={1900} height={400}/>
+            <div className={"flex flex-col gap-1"}>
+                <h3 className={"bg-gray-100 dark:bg-gray-900 rounded-sm rounded-t-lg text-xl font-semibold p-2"}>Tickets</h3>
+                <div className={"bg-gray-100 dark:bg-gray-900 rounded-sm p-2"}>
+                    <p>Prijs VVK: €{event.tickets.prijsVVK}</p>
+                    <p>Prijs ADK: €{event.tickets.prijsADK}</p>
+                </div>
+                <Link className={"bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-b-lg rounded-sm p-2"} href={event.tickets.onlineURL} target={"_blank"}>Koop tickets</Link>
+            </div>
+            <div className={"flex flex-col gap-1"}>
+                <h3 className={"bg-gray-100 dark:bg-gray-900 rounded-sm rounded-t-lg text-xl font-semibold p-2"}>Aanvang &amp; einde</h3>
+                <div className={"bg-gray-100 dark:bg-gray-900 rounded-sm p-2"}>
+                    <p>Aanvang: {event.start.toLocaleString("nl-BE", {dateStyle: "long", timeStyle: "short"})}</p>
+                    <p>Einde: {event.end.toLocaleString("nl-BE", {dateStyle: "long", timeStyle: "short"})}</p>
+                </div>
+                <p className={"bg-gray-100 dark:bg-gray-900 rounded-b-lg rounded-sm uppercase p-2"} lang={"en-US"}>In = in / out = out</p>
+            </div>
+            <div className={"flex flex-col gap-1"}>
+                <h3 className={"bg-gray-100 dark:bg-gray-900 rounded-sm rounded-t-lg text-xl font-semibold p-2"}>Locatie</h3>
+                <div className={"bg-gray-100 dark:bg-gray-900 rounded-b-lg rounded-sm flex-grow p-2"}>
+                    <p>{event.location.name}</p>
+                    <p>{event.entry.adres.streetAndNum},</p>
+                    <p>{event.entry.adres.postalCode} {event.entry.adres.City}</p>
+                </div>
+            </div>
         </section>
     </main>;
 }
