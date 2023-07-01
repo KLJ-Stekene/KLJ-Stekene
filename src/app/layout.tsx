95576b0d4d4a4a977a9f41739478aa453772254e
Jens Penneman <jenspenneman26@gmail.com>
2023-07-01 18:49:25 +0200

    Facebook en Instagram added to Footer.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,29 +35,25 @@ export const metadata = {
     ],
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
-    return (
-        <html lang="nl">
-        <body className={inter.className}>
-        <header className={"sticky top-0 z-40 w-full container mx-auto flex flex-row p-2 gap-1 print:hidden"}>
-            <Link href={"/"} className={"bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 flex-grow-0 flex-shrink-0 rounded-s-lg rounded-e-sm h-14 p-2 aspect-square"}>
-                <Image src={"/KLJ.svg"} alt={"Het KLJ logo"} width={1} height={1} className={"w-full h-full"}/>
-            </Link>
-            <div className={"bg-blue-100 dark:bg-blue-900 flex-grow flex-shrink rounded-sm overflow-x-auto no-scrollbar flex gap-1"}>
-                <Link href={"/contact"} className={"hover:bg-blue-200 dark:hover:bg-blue-800 rounded-sm p-4 uppercase"}>Contact</Link>
-                <Link href={"/evenementen"} className={"hover:bg-blue-200 dark:hover:bg-blue-800 rounded-sm p-4 uppercase"}>Evenementen</Link>
-            </div>
-            <Link href={"mailto:allebestuur@kljstekene.be"} target={"_blank"} className={"bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 flex-grow-0 flex-shrink-0 rounded-s-sm rounded-e-lg h-14 flex items-center px-4 py-2 uppercase"}>Mail</Link>
-        </header>
-        {children}
-        <footer className={"w-full container mx-auto p-2 print:hidden"}>
-            <p className={"bg-gray-100 dark:bg-gray-900 rounded-lg w-full p-2 font-extralight"}>Created by <Link href={"https://web-4-you.be"} target={"_blank"}>WEB4YOU</Link></p>
-        </footer>
-        </body>
-        </html>
-    );
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return <html lang="nl">
+    <body className={inter.className}>
+    <header className={"sticky top-0 z-40 w-full container mx-auto flex flex-row p-2 gap-1 print:hidden"}>
+        <Link href={"/"} className={"bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 flex-grow-0 flex-shrink-0 rounded-s-lg rounded-e-sm h-14 p-2 aspect-square"}>
+            <Image src={"/KLJ.svg"} alt={"Het KLJ logo"} width={1} height={1} className={"w-full h-full"}/>
+        </Link>
+        <div className={"bg-blue-100 dark:bg-blue-900 flex-grow flex-shrink rounded-sm overflow-x-auto no-scrollbar flex gap-1"}>
+            <Link href={"/contact"} className={"hover:bg-blue-200 dark:hover:bg-blue-800 rounded-sm p-4 uppercase"}>Contact</Link>
+            <Link href={"/evenementen"} className={"hover:bg-blue-200 dark:hover:bg-blue-800 rounded-sm p-4 uppercase"}>Evenementen</Link>
+        </div>
+        <Link href={"mailto:allebestuur@kljstekene.be"} target={"_blank"} className={"bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 flex-grow-0 flex-shrink-0 rounded-s-sm rounded-e-lg h-14 flex items-center px-4 py-2 uppercase"}>Mail</Link>
+    </header>
+    {children}
+    <footer className={"w-full container mx-auto flex flex-col md:flex-row-reverse gap-1 p-2 print:hidden"}>
+        <Link className={"bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-sm rounded-t-lg md:rounded-t-sm md:rounded-r-lg p-2"} href={"https://www.facebook.com/KljStekene"} target={"_blank"}>Facebook</Link>
+        <Link className={"bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-sm p-2"} href={"https://www.instagram.com/kljstekene/"} target={"_blank"}>Instagram</Link>
+        <p className={"bg-gray-100 dark:bg-gray-900 flex-grow rounded-sm rounded-b-lg md:rounded-b-sm md:rounded-l-lg p-2"}>Created by <Link className={"hover:bg-gray-200 dark:hover:bg-gray-800 rounded-lg p-2 -m-2 hover:ml-0 transition-all duration-300"} href={"https://web-4-you.be/"} target={"_blank"}>WEB4YOU</Link></p>
+    </footer>
+    </body>
+    </html>;
 }
