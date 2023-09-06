706104ed5468191930033b75cd73433297573395
Jens Penneman <jenspenneman26@gmail.com>
2023-09-06 12:53:56 +0200

    Added homepage + small boardmember.tsx changes
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- components/contact/boardmember.tsx
+++ components/contact/boardmember.tsx
@@ -1,5 +1,6 @@
 import {StaticImport} from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import Link from "next/link";
 
 interface BoardmemberProps {
     firstname: string;
@@ -11,7 +12,11 @@ interface BoardmemberProps {
 export default async function Boardmember({firstname, lastname, avatar, tasklist}: BoardmemberProps) {
     return <div className={"flex flex-col gap-1"}>
         {avatar && <Image src={avatar} alt={firstname + "'s gezicht"} className={"first:rounded-t-lg rounded-sm w-full"} width={380} height={380}/>}
-        <h3 className={"bg-gray-100 dark:bg-gray-900 first:rounded-t-lg rounded-sm p-2 text-xl font-semibold"}>{firstname} {lastname}</h3>
+        <div className={"flex flex-row gap-1"}>
+            <h3 className={"bg-gray-100 dark:bg-gray-900 rounded-sm flex-grow p-2 text-xl font-semibold"}>{firstname} {lastname}</h3>
+            <Link href={`mailto:${firstname}.${lastname}@kljstekene.be`} target={"_blank"}
+                  className={"bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-sm p-2 px-4 uppercase"}>Mail</Link>
+        </div>
         {tasklist && <ul className={"bg-gray-100 dark:bg-gray-900 rounded-sm last:rounded-b-lg flex flex-col flex-grow gap-1"}>
             {tasklist?.map((task, index) => <BoardmembersTask key={index} name={task}/>)}
         </ul>}
--- components/contact/boardmembers-group.tsx
+++ components/contact/boardmembers-group.tsx
@@ -14,7 +14,7 @@ export default async function BoardmembersGroup({name, email, avatar}: Boardmemb
             <Image src={avatar} alt={name + "'s group"} className={"object-cover rounded-t-lg rounded-sm"} fill/>
         </div>}
         <div className={"flex flex-row gap-1"}>
-            <h3 className={"bg-gray-100 dark:bg-gray-900 flex-grow rounded-sm rounded-b-lg rounded-br-sm p-2 text-xl font-semibold text-center"}>{name}</h3>
+            <h3 className={"bg-gray-100 dark:bg-gray-900 flex-grow rounded-sm rounded-b-lg rounded-br-sm p-2 text-xl font-semibold"}>{name}</h3>
             {email && <Link href={"mailto:" + email} target={"_blank"}
                             className={"bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-sm rounded-br-lg p-2 px-4 uppercase"}>Mail</Link>}
         </div>
